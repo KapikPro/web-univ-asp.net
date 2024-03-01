eddcd3971da58ee7fbf3da176bfe3c3b289c8a65
Yaroslav Senchin <yrik200315@gmail.com>
2024-03-01 19:40:29 +0300

    made first lab

---
+++ lab_1/WebApplication2/Models/Form.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Form
+    {
+        public string Name { get; set; }
+        public string Secondname { get; set; }
+        public string Surname { get; set; }
+    }
+}

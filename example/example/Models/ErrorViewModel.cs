5605018a6245d4e3b32776b092ece56a65e2a804
Yaroslav Senchin <yrik200315@gmail.com>
2024-04-25 23:33:43 +0300

    lab 3 created

---
+++ example/example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

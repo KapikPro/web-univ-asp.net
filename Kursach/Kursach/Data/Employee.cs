4272647bff1f3221f4d6b35c12301b03c574116f
Yaroslav Senchin <yrik200315@gmail.com>
2024-05-01 20:18:57 +0300

    kursach relocated

--- Kursach/Kursach/Data/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kursach.Data
-{
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Title { get; set; }
-    }
-}
